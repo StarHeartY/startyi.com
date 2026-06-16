ae86cce0ecfab17a19837e64bc325ed767fb5c80
StarHeartY <yirui.pre@gmail.com>
2026-06-16 19:20:56 +0800

    修改成静态网页

--- next.config.mjs
+++ next.config.mjs
@@ -3,32 +3,16 @@ import { withContentCollections } from "@content-collections/next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
-          },
-        ],
-      },
-    ];
+
+  output: "export",// 开启纯静态导出模式
+
+  // 静态模式不支持 Next.js 自带的动态图片优化 API
+  images: {
+    unoptimized: true,
   },
+
+  // 注意：原有的 async headers() 已经被移除，因为静态导出不支持该功能。
 };
 
-// withContentCollections must be the outermost plugin
+// withContentCollections 必须保留作为最外层插件
 export default withContentCollections(nextConfig);
